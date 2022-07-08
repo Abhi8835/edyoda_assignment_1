3292d6948c8e7c602f33664ff8a73b60eda47ec1
Abhi8835 <108941776+Abhi8835@users.noreply.github.com>
2022-07-08 19:56:48 +0530

    initial commit

---
+++ two_datatypes.py
@@ -0,0 +1,51 @@
+# int
+# immutable
+num=-20
+print(type(num))
+
+#float
+# immutable
+price= 67.20
+print(type(price))
+
+#str
+# immutable
+name= "bharti"
+print(type(name))
+
+#bool
+# immutable
+human= True
+print(type(human))
+
+
+#complex
+# immutable
+comp = 7+9j
+print(type(comp))
+
+#list
+# mutable
+lst = [5,6,7,8,9,1]
+print(lst)
+print(type(lst))
+
+#tuple
+# immutable
+tup = (4,5,6,7,"edyoda")
+print(type(tup))
+
+#set
+# mutable
+set_demo = {4,5,6,True}
+print(type(set_demo))
+
+#dict
+# mutable
+dict_demo = {"one":"bharti","two":"mohit"}
+print(type(dict_demo))
+
+#None
+language = None
+print(language)
+print(type(language))
